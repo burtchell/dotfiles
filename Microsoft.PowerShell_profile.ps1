4b925bc35308d611c1dec88a9ee5c6948baf3d79
Brandon Burtchell <bab334@txstate.edu>
2022-04-18 12:24:11 -0500

    cleaning up prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,3 @@
-#oh-my-posh init pwsh --config ~/.jandedobbeleer.omp.json | Invoke-Expression
 oh-my-posh init pwsh --config ~/configs/omp.json | Invoke-Expression
 
 Import-Module -Name Terminal-Icons
