e1c50db749d6230adf1fe52f1e5976eb6d7aa860
Brandon Burtchell <bab334@txstate.edu>
2022-04-18 14:54:39 -0500

    added aliases to pwsh, removed unnecessary omp segments, renamed file

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,17 @@
-oh-my-posh init pwsh --config ~/configs/omp.json | Invoke-Expression
+# copy-paste this file or text to the location of the profile (i.e. ~/Documents/PowerShell)
 
+# oh-my-posh (note file location. this repo should be cloned there.)
+oh-my-posh init pwsh --config ~/configs/burtchell.omp.json | Invoke-Expression
+
+# terminal icons
 Import-Module -Name Terminal-Icons
 
-Set-PSReadLineOption -PredictionSource History
+# PSReadLine
 Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History
+
+# aliases
+Set-Alias g git
+Set-Alias exp explorer
+Set-Alias grep findstr
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # configs
+
 Personal config files saved in this repository to make environment customization easier on multiple machines.
