d1e7badc69bda1c7701c66440bae65e2d00a4ea8
Brandon Burtchell <brandonburtchell@gmail.com>
2022-04-20 16:28:06 -0500

    added more git aliases to pwsh

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -11,8 +11,30 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -PredictionSource History
 
-# aliases
+# git aliases
 Set-Alias g git
+
+function GitStatus() { & git status $args }
+Set-Alias -Name 'gs' -Value 'GitStatus' -Scope Global
+
+function GitFetch() { & git fetch $args }
+Set-Alias -Name 'gf' -Value 'GitFetch' -Scope Global
+
+function GitAdd() { & git add $args }
+Set-Alias -Name 'ga' -Value 'GitAdd' -Scope Global
+
+function GitAddAll() { & git add -A }
+Set-Alias -Name 'gaa' -Value 'GitAddAll' -Scope Global
+
+function GitCommit() { & git commit -m $args } # note: requires a message (best practice)
+Remove-Item 'Alias:\gc' -Force # override an alias for Get-Content cmdlet
+Set-Alias -Name 'gc' -Value 'GitCommit' -Scope Global
+
+function GitPush() { & git push $args }
+Remove-Item 'Alias:\gp' -Force # override an alias for Get-ItemProperty cmdlet
+Set-Alias -Name 'gp' -Value 'GitPush' -Scope Global
+
+# misc. aliases
 Set-Alias exp explorer
 Set-Alias grep findstr
 Set-Alias ll dir
