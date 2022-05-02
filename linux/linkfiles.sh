1ce2fd0c4893a52adae119747d0fd608548bf006
Brandon Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2022-05-02 10:37:39 -0500

    Add aliases, change file name .bash_aliases -> .aliases

--- linux/linkfiles.sh
+++ linux/linkfiles.sh
@@ -3,6 +3,6 @@
 # remove and link dotfiles to this repo
 ln -sf ~/dotfiles/linux/.profile ~/.profile
 ln -sf ~/dotfiles/linux/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/linux/.bash_aliases ~/.bash_aliases
+ln -sf ~/dotfiles/linux/.aliases ~/.aliases
 ln -sf ~/dotfiles/linux/.hushlogin ~/.hushlogin
 ln -sf ~/dotfiles/.config/neofetch/config.conf ~/.config/neofetch/config.conf
