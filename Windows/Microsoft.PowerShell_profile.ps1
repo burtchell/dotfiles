237a5ddac4fc6e4b1f6d965916e4c2643a0b1b1a
Brandon Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2022-05-27 13:09:53 -0500

    Change pwsh profile to source aliases and extra from seperate files

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,15 @@
+# The default PowerShell profile. Symbolically linked by ~/dotfiles/linkfiles.ps1.
+
+# source aliases
+. ~/dotfiles/windows/aliases.ps1
+
+# source extra
+If (Test-Path -Path ~/extra.ps1) {
+  . ~/extra.ps1
+}
+
 # init oh-my-posh
 oh-my-posh init pwsh --config ~/dotfiles/burtchell.simple.omp.json | Invoke-Expression
-
-neofetch
 
 # import terminal icons
 Import-Module -Name Terminal-Icons
@@ -11,39 +19,5 @@ Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -PredictionSource History
 
-# git aliases
-Set-Alias -Name 'g' -Value 'git' -Scope Global
-function global:GitStatus() { & git status $args }
-Set-Alias -Name 'gs' -Value 'GitStatus' -Scope Global
-function global:GitFetch() { & git fetch $args }
-Set-Alias -Name 'gf' -Value 'GitFetch' -Scope Global
-function global:GitAdd() { & git add $args }
-Set-Alias -Name 'ga' -Value 'GitAdd' -Scope Global
-function global:GitAddAll() { & git add -A }
-Set-Alias -Name 'gaa' -Value 'GitAddAll' -Scope Global
-function global:GitCommit() { & git commit -m $args } # note: requires a message (best practice)
-Remove-Item 'Alias:\gc' -Force # override an alias for Get-Content cmdlet
-Set-Alias -Name 'gc' -Value 'GitCommit' -Scope Global
-function global:GitPush() { & git push $args }
-Remove-Item 'Alias:\gp' -Force # override an alias for Get-ItemProperty cmdlet
-Set-Alias -Name 'gp' -Value 'GitPush' -Scope Global
-
-function global:ReloadPowerShell() { & $PROFILE }
-Set-Alias -Name 'reload' -Value 'ReloadPowerShell' -Scope Global
-
-# open windows explorer
-Set-Alias -Name 'exp' -Value 'explorer' -Scope Global
-Set-Alias -Name 'open' -Value 'explorer' -Scope Global
-
-Set-Alias -Name 'grep' -Value 'findstr' -Scope Global
-
-# ls variations (for consistency with linux muscle-memory)
-Set-Alias -Name 'ls' -Value 'dir' -Scope Global
-Set-Alias -Name 'll' -Value 'dir' -Scope Global
-function global:ListTreeFiles() { & tree /F /a $args }
-Set-Alias -Name 'lt' -Value 'ListTreeFiles' -Scope Global
-Set-Alias -Name 'la' -Value 'dir' -Scope Global
-
-# jekyll site local launch with live reload
-function global:JekyllExec() { & bundle exec jekyll serve --livereload $args }
-Set-Alias -Name 'exek' -Value 'JekyllExec' -Scope Global
+# run neofetch on startup
+neofetch
