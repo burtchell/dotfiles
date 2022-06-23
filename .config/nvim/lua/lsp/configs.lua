88f23525e0901e64518dcacd5804b4536cfa37b9
Brandon Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2022-06-23 11:15:31 -0500

    Figure out why lsp wasn't working, must list servers in configs.lua
    Will look into getting all servers using a function in the future

--- .config/nvim/lua/lsp/configs.lua
+++ .config/nvim/lua/lsp/configs.lua
@@ -8,18 +8,30 @@ end
 
 local lspconfig = require("lspconfig")
 
--- add lsp extensions with special settings here
-local servers = { "jsonls", "sumneko_lua" }
+-- add all servers here:
+local servers = {
+  "jsonls",
+  "sumneko_lua",
+  "tsserver",
+  "cssls",
+  "cssmodules_ls",
+  "html",
+  "emmet_ls",
+  "pyright"
+}
 
 lsp_installer.setup {
 	ensure_installed = servers
 }
 
 for _, server in pairs(servers) do
+  -- apply global configs (in lsp.handlers) to all installed servers
 	local opts = {
 		on_attach = require("lsp.handlers").on_attach,
 		capabilities = require("lsp.handlers").capabilities,
 	}
+
+  -- if server has a settings file, apply those settings as well
 	local has_custom_opts, server_custom_opts = pcall(require, "lsp.settings." .. server)
 	if has_custom_opts then
 	 	opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
