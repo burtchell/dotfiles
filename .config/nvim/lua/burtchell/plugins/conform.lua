51ac596c43d8f31d559d8c0dde5c1829a5a3c7d9
Brandon Alexander Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2024-03-30 18:32:53 -0500

    Update diagonistics icons, git sign placement on tree, and turn off autoformat on save

--- .config/nvim/lua/burtchell/plugins/conform.lua
+++ .config/nvim/lua/burtchell/plugins/conform.lua
@@ -24,11 +24,11 @@ return {
 				python = { "black" },
 				["_"] = { "trim_whitespace" }, -- filetypes that don't have another formatter configured
 			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			},
+			-- format_on_save = {
+			-- 	lsp_fallback = true,
+			-- 	async = false,
+			-- 	timeout_ms = 1000,
+			-- },
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
--- .config/nvim/lua/burtchell/plugins/lualine.lua
+++ .config/nvim/lua/burtchell/plugins/lualine.lua
@@ -1,95 +1,95 @@
 -- https://github.com/nvim-lualine/lualine.nvim
 
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = {
-    'nvim-tree/nvim-web-devicons'
-  },
+	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
 
-  config = function()
-    local lualine = require("lualine")
-    
-    local hide_in_width = function()
-      return vim.fn.winwidth(0) > 80
-    end
+	config = function()
+		local lualine = require("lualine")
 
-    local diagnostics = {
-      "diagnostics",
-      sources = { "nvim_diagnostic" },
-      sections = { "error", "warn" },
-      symbols = { error = " ", warn = " " },
-      colored = false,
-      update_in_insert = false,
-      always_visible = false,
-    }
+		local hide_in_width = function()
+			return vim.fn.winwidth(0) > 80
+		end
 
-    local diff = {
-      "diff",
-      colored = false,
-      symbols = { added = "+", modified = "~", removed = "-" }, -- changes diff symbols
-      cond = hide_in_width
-    }
+		local diagnostics = {
+			"diagnostics",
+			sources = { "nvim_diagnostic" },
+			sections = { "error", "warn" },
+			symbols = { error = " ", warn = " " },
+			colored = false,
+			update_in_insert = false,
+			always_visible = false,
+		}
 
-    local fileformat = {
-      'fileformat',
-      symbols = {
-        unix = '', -- e712
-        dos = '',  -- e70f
-        mac = '',  -- e711
-      },
-    }
+		local diff = {
+			"diff",
+			colored = false,
+			symbols = { added = "+", modified = "~", removed = "-" }, -- changes diff symbols
+			cond = hide_in_width,
+		}
 
-    local filetype = {
-      "filetype",
-      colored = false,
-      icons_enabled = true,
-      icon_only = true,
-      icon = nil,
-    }
+		local fileformat = {
+			"fileformat",
+			symbols = {
+				unix = "", -- e712
+				dos = "", -- e70f
+				mac = "", -- e711
+			},
+		}
 
-    local branch = {
-      "branch",
-      icons_enabled = true,
-      icon = "",
-    }
+		local filetype = {
+			"filetype",
+			colored = false,
+			icons_enabled = true,
+			icon_only = true,
+			icon = nil,
+		}
 
-    local location = {
-      "location",
-      padding = { left = 1, right = 1 },
-    }
+		local branch = {
+			"branch",
+			icons_enabled = true,
+			icon = "",
+		}
 
-    -- local spaces = function()
-    -- 	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
-    -- end
+		local location = {
+			"location",
+			padding = { left = 1, right = 1 },
+		}
 
-    lualine.setup({
-      options = {
-        icons_enabled = true,
-        theme = "auto",
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
-        always_divide_middle = true,
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { branch, diagnostics },
-        lualine_c = { "filename" },
-        -- lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_x = { diff, fileformat, filetype },
-        lualine_y = { "progress" },
-        lualine_z = { location },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-      tabline = {},
-      extensions = {},
-    })
-  end
+		-- local spaces = function()
+		-- 	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+		-- end
+
+		lualine.setup({
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+				always_divide_middle = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { branch, diagnostics },
+				-- lualine_c = { "filename" },
+				-- lualine_x = { "encoding", "fileformat", "filetype" },
+				lualine_x = { diff, fileformat, filetype },
+				lualine_y = { "progress" },
+				lualine_z = { location },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {},
+			extensions = {},
+		})
+	end,
 }
