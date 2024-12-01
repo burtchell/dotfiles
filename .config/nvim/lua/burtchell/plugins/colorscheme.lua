2a31f4f0ae660c9a9e2341da53b6a645be8ee693
Brandon Alexander Burtchell <62345978+burtchell@users.noreply.github.com>
2024-12-01 11:56:05 -0600

    Switch to gruvbox, simplify tmux

--- .config/nvim/lua/burtchell/plugins/colorscheme.lua
+++ .config/nvim/lua/burtchell/plugins/colorscheme.lua
@@ -1,9 +1,20 @@
 -- https://github.com/shaunsingh/nord.nvim
 
+-- return {
+--   "shaunsingh/nord.nvim",
+--   priority = 1000,  -- ensure this loads first
+--   config = function()
+--     vim.cmd([[colorscheme nord]])
+--   end
+-- }
+
+-- https://github.com/ellisonleao/gruvbox.nvim
+
 return {
-  "shaunsingh/nord.nvim",
-  priority = 1000,  -- ensure this loads first
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
   config = function()
-    vim.cmd([[colorscheme nord]])
+    vim.cmd([[colorscheme gruvbox]])
   end
+  -- opts = ...
 }
