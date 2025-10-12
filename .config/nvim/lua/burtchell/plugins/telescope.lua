075e0d71e70ce430e83eb5bb3bee039f2d00a9da
Brandon Alexander Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2025-10-12 17:26:12 -0500

    Minor tweaks

--- .config/nvim/lua/burtchell/plugins/telescope.lua
+++ .config/nvim/lua/burtchell/plugins/telescope.lua
@@ -28,9 +28,6 @@ return {
           "node_modules",
         },
         mappings = {
-          n = {
-            [";;"] = actions.close
-          },
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
