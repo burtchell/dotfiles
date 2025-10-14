52e5624996bc2e4e38fae17db7fc78b8dbf7775f
Brandon Alexander Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2025-10-14 18:08:51 -0500

    Cleanup

--- .config/nvim/lua/burtchell/plugins/alpha.lua
+++ .config/nvim/lua/burtchell/plugins/alpha.lua
@@ -8,12 +8,6 @@ return {
     local dashboard = require("alpha.themes.dashboard")
 
     dashboard.section.header.val = {
-      -- [[                               __                ]],
-      -- [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-      -- [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-      -- [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-      -- [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-      -- [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
       [[                __                ]],
       [[  ___   __  __ /\_\    ___ ___    ]],
       [[ / _ `\/\ \/\ \\/\ \  / __` __`\  ]],
@@ -25,7 +19,6 @@ return {
       dashboard.button("f", "Find file", ":Telescope find_files <CR>"),
       dashboard.button("n", "Browse filetree", ":NvimTreeToggle <CR>"),
       dashboard.button("e", "New file", ":ene <BAR> startinsert <CR>"),
-      -- dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
       dashboard.button("t", "Find text", ":Telescope live_grep <CR>"),
       dashboard.button("c", "Configuration", ":e ~/.config/nvim/init.lua <CR>"),
       dashboard.button("q", "Quit Neovim", ":qa<CR>"),
