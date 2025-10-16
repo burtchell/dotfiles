a40adab98b5baa08cbe1dd3b073323c8c99b1cc2
Brandon Alexander Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2025-10-16 09:21:00 -0500

    Add fish path editing functions and fix issue with conda init path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -27,13 +27,13 @@ zoxide init fish | source
 # for macOS
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-if test -f /home/brandon/miniconda3/bin/conda
-    eval /home/brandon/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+if test -f /Users/brandon/miniconda3/bin/conda
+    eval /Users/brandon/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 else
-    if test -f "/home/brandon/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/home/brandon/miniconda3/etc/fish/conf.d/conda.fish"
+    if test -f "/Users/brandon/miniconda3/etc/fish/conf.d/conda.fish"
+        . "/Users/brandon/miniconda3/etc/fish/conf.d/conda.fish"
     else
-        set -x PATH "/home/brandon/miniconda3/bin" $PATH
+        set -x PATH "/Users/brandon/miniconda3/bin" $PATH
     end
 end
 # <<< conda initialize <<<
--- .config/fish/functions/aliases.fish
+++ .config/fish/functions/aliases.fish
@@ -1,5 +1,20 @@
 # fish shell aliases
 
+function addpaths
+    contains -- $argv $fish_user_paths
+       or set -U fish_user_paths $fish_user_paths $argv
+    echo "Updated PATH: $PATH"
+end
+
+function removepath
+    if set -l index (contains -i $argv[1] $PATH)
+        set --erase --universal fish_user_paths[$index]
+        echo "Updated PATH: $PATH"
+    else
+        echo "$argv[1] not found in PATH: $PATH"
+    end
+end
+
 # Allow aliases to be sudo'ed
 alias sudo="sudo "
 
