c35157324b27677450c318f37daa0fffe0b751dc
Brandon Alexander Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2025-10-16 11:19:41 -0500

    Have conda activate on-command to speed up shell launch

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,41 +16,24 @@ end
 zoxide init fish | source
 
 . ~/.config/fish/functions/aliases.fish
-# . ~/.config/fish/functions/keychain.fish
 [ -r ~/.extra ] && [ -f ~/.extra ] && bash ~/.extra
 
-# force arrow key word movement in terminal (gnome terminal interferes)
-# bind \e\[1\;5C forward-word
-# bind \e\[1\;5D backward-word
-# bind \cH backward-kill-word
-
-# for macOS
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/brandon/miniconda3/bin/conda
-    eval /Users/brandon/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/Users/brandon/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/Users/brandon/miniconda3/etc/fish/conf.d/conda.fish"
+# load conda on-command to prevent slow shell startup
+function conda
+    functions --erase conda
+    if test -f $HOME/miniconda3/bin/conda
+        eval $HOME/miniconda3/bin/conda "shell.fish" "hook" | source
     else
-        set -x PATH "/Users/brandon/miniconda3/bin" $PATH
+        if test -f "$HOME/miniconda3/etc/fish/conf.d/conda.fish"
+            . "$HOME/miniconda3/etc/fish/conf.d/conda.fish"
+        else
+            set -x PATH "$HOME/miniconda3/bin" $PATH
+        end
+    end
+    if type -q conda
+        conda $argv
     end
 end
-# <<< conda initialize <<<
-
-# for linux
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/brandon/miniconda3/bin/conda
-    eval /home/brandon/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/home/brandon/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/home/brandon/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/home/brandon/miniconda3/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
 
 # anything else that may be system specific:
 if test -f ~/.config/fish/functions/extra.fish
