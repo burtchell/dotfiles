3d064dc439203e1e4cc6bb99c11bd5e9154fc465
Brandon Alexander Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2026-01-16 11:34:16 -0600

    Add signs_staged symbols

--- .config/nvim/lua/burtchell/plugins/gitsigns.lua
+++ .config/nvim/lua/burtchell/plugins/gitsigns.lua
@@ -14,6 +14,15 @@ return {
         delete = { text = '_' },
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
+        untracked    = { text = '┆' },
+      },
+      signs_staged = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked    = { text = '┆' },
       },
       current_line_blame = false,
     })
