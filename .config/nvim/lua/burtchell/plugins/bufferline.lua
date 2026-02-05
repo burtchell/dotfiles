61ce80cce4a9b0245ae26f43bacb62915c230a95
Brandon Alexander Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2026-02-05 12:06:55 -0600

    Simplify bufferline config and fix weird artifact when multiple buffers are open across windows

--- .config/nvim/lua/burtchell/plugins/bufferline.lua
+++ .config/nvim/lua/burtchell/plugins/bufferline.lua
@@ -11,81 +11,33 @@ return {
     bufferline.setup({
       options = {
         offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
+        style_preset = bufferline.style_preset.minimal,
+        color_icons = true,
       },
       highlights = {
-        fill = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        background = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        buffer_visible = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-
-        close_button = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        close_button_visible = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-
-        tab_selected = {
-          fg = { attribute = "fg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "Normal" },
-        },
-        tab = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        tab_close = {
-          fg = { attribute = "fg", highlight = "TabLineSel" },
-          bg = { attribute = "bg", highlight = "Normal" },
-        },
+        -- see :help bufferline-highlights
 
         -- the color of the path of a buffer with a duplicate filename
         duplicate_selected = {
-          fg = { attribute = "fg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "Normal" },
           italic = true,
         },
         duplicate_visible = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
           italic = true,
         },
         duplicate = {
-          fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
           italic = true
         },
 
         modified = {
           fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
         },
         modified_selected = {
           fg = { attribute = "fg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "Normal" },
         },
         modified_visible = {
           fg = { attribute = "fg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
         },
 
-        separator = {
-          fg = { attribute = "bg", highlight = "TabLine" },
-          bg = { attribute = "bg", highlight = "TabLine" },
-        },
-        separator_selected = {
-          fg = { attribute = "bg", highlight = "Normal" },
-          bg = { attribute = "bg", highlight = "Normal" },
-        },
         indicator_selected = {
           fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" },
           bg = { attribute = "bg", highlight = "Normal" },
