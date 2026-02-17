3b01f21f4d7004f358970a7e42cd490083268e47
Brandon Alexander Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2026-02-17 17:49:11 -0600

    Add icons back to sign column with proper highlighting

--- .config/nvim/lua/burtchell/plugins/lsp/nvim-lspconfig.lua
+++ .config/nvim/lua/burtchell/plugins/lsp/nvim-lspconfig.lua
@@ -10,34 +10,33 @@ return {
   },
   config = function()
     vim.diagnostic.config({
-      signs = false,
-      -- signs = {
-      --   text = {
-      --     [vim.diagnostic.severity.ERROR] = 'x',
-      --     [vim.diagnostic.severity.WARN] = '!',
-      --     [vim.diagnostic.severity.HINT] = 'i',
-      --     [vim.diagnostic.severity.INFO] = '?',
-      --   },
-      -- },
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = 'E',
+          [vim.diagnostic.severity.WARN] = 'W',
+          [vim.diagnostic.severity.HINT] = 'H',
+          [vim.diagnostic.severity.INFO] = 'I',
+        },
+      },
       virtual_text = false, -- disable in-line virtual text
       update_in_insert = true,
       underline = true,
       severity_sort = true,
-      float = {
-        focusable = false,
-        style = "minimal",
-        border = "rounded",
-        source = true,
-        header = "",
-        prefix = "",
-      },
+      -- float = {
+      --   focusable = false,
+      --   style = "minimal",
+      --   border = "rounded",
+      --   source = true,
+      --   header = "",
+      --   prefix = "",
+      -- },
     })
 
-    -- attempted to set bg highlight of sign-column diagnostics to none but it overrided the fg color too
-    -- vim.api.nvim_set_hl(0, "DiagnosticSignError", { bg = nil })
-    -- vim.api.nvim_set_hl(0, "DiagnosticSignWarn",  { bg = nil })
-    -- vim.api.nvim_set_hl(0, "DiagnosticSignInfo",  { bg = nil })
-    -- vim.api.nvim_set_hl(0, "DiagnosticSignHint",  { bg = nil })
+    -- default highlights don't have a transparent bg. linking with highlights somehow fixes this
+    vim.api.nvim_set_hl(0, "DiagnosticSignError", { link = "DiagnosticError" })
+    vim.api.nvim_set_hl(0, "DiagnosticSignWarn",  { link = "DiagnosticWarn" })
+    vim.api.nvim_set_hl(0, "DiagnosticSignInfo",  { link = "DiagnosticInfo" })
+    vim.api.nvim_set_hl(0, "DiagnosticSignHint",  { link = "Comment" })
 
     -- manually config any special LSP servers
     vim.lsp.config("lua_ls", {
