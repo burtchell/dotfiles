f23341b56190c1cfc34f6d16643dfb1cd3f11ae6
Brandon Alexander Burtchell <62345978+dukeofjukes@users.noreply.github.com>
2026-04-01 13:44:50 -0500

    Remove relative lines

--- .config/nvim/lua/burtchell/core/autocommands.lua
+++ .config/nvim/lua/burtchell/core/autocommands.lua
@@ -62,15 +62,15 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- show relativenumber in non-insert modes
-vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
-  pattern = "*",
-  group = augroup,
-  callback = function()
-    if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
-      vim.opt.relativenumber = true
-    end
-  end,
-})
+-- vim.api.nvim_create_autocmd({ "BufEnter", "FocusGained", "InsertLeave", "CmdlineLeave", "WinEnter" }, {
+--   pattern = "*",
+--   group = augroup,
+--   callback = function()
+--     if vim.o.nu and vim.api.nvim_get_mode().mode ~= "i" then
+--       vim.opt.relativenumber = true
+--     end
+--   end,
+-- })
 
 -- turn off relativenumber in insert mode
 vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost", "InsertEnter", "CmdlineEnter", "WinLeave" }, {
